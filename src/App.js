506d4d0aeb029f999559021acfcbc7d0dc0cee7c
Marco-Velazz <137120740+Marco-Velazz@users.noreply.github.com>
2025-09-13 17:51:54 -0400

    Created a working loading page with an animated gif

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import Map from './components/Map'
+import { useState, useEffect } from 'react';
+import Map from './components/Map';
+import Loader from './components/Loader';
 
 function App() {
+
+  // Array to store events
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    // Fetch events from NASA EONET API
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events');
+        const data = await response.json();
+
+        // Mapping data
+        const formattedEvents = data.events.map(event => ({
+          id: event.id,
+          title: event.title,
+          categories: event.categories,
+          sources: event.sources,
+          geometry: event.geometries, // contains coordinates and date
+          link: event.link
+        }));
+
+        console.log('Fetched events:', formattedEvents);
+
+        setEvents(formattedEvents);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <div>
-      <Map />
+      { !loading ?  <Map events={events} /> : <Loader /> }
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,20 @@
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import LocationMarker from "./LocationMarker";
 
-const containerStyle = {
-  width: "100vw",
-  height: "100vh",
-};
-
-const center = {
-  // Example: San Francisco
-  lat: 37.7749,
-  lng: -122.4194,
-};
+const containerStyle = { width: "100vw", height: "100vh" };
+const center = { lat: 37.7749, lng: -122.4194 };
 
 function Map() {
+
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center} 
-        zoom={10}
-      >
-      <Marker 
-        position={center} 
-        icon={{
-        url: "https://img.icons8.com/color/48/000000/fire-element.png", // example
-        scaledSize: new window.google.maps.Size(40, 40)
-        }}
-      />
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        <Marker
+          position={center}
+          icon={{
+            url: "https://img.icons8.com/color/48/000000/fire-element.png",
+            scaledSize: { width: 40, height: 40 },
+          }}
+        />
       </GoogleMap>
     </LoadScript>
   );
